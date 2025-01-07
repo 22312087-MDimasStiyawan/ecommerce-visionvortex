d0a6ae7ebfe2f8ba879cb8bdeeaa08af126433e8
Rifki Abdullah <rifkiabdullah168@gmail.com>
2025-01-08 00:46:37 +0700

    Rifki: Membuat Form Komponen pada tampilan( admin)

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,14 +1,44 @@
 "use client"
+// Mengimpor semua ekspor dari pustaka "zod" ke dalam namespace "z".
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 // Mengimpor hook `useStoreModal` yang digunakan untuk mengakses state global modal dari Zustand.
 import { useStoreModal } from "@/hooks/use-store-modal"
 // Mengimpor komponen `Modal` dari path lokal. Komponen ini bertugas untuk menampilkan dialog/modal.
-import { Modal } from "../ui/modal";
+import { Modal } from "@/components/ui/modal";
+import { 
+    Form, 
+    FormControl, 
+    FormField, 
+    FormItem, 
+    FormLabel, 
+    FormMessage 
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+    name: z.string().min(1),
+});
 
 // Mendefinisikan komponen fungsional React bernama `StoreModal`.
  export const StoreModal = () => {
     // Mengakses state dan fungsi dari store modal menggunakan hook `useStoreModal`.
     const storeModal = useStoreModal();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+        },
+    });
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        console.log(values);
+        // TODO: Create Store
+    }
 
     return(
         <Modal
@@ -17,9 +47,34 @@ import { Modal } from "../ui/modal";
         isOpen={storeModal.isOpen}
         onClose={storeModal.onClose}
         >
-            future Create Sore Form
-            {/* Konten modal yang akan muncul di dalamnya. Saat ini berupa placeholder teks, 
-                tetapi akan digantikan dengan form atau elemen lain di masa mendatang. */}
+            <div>
+                <div className="space-y-4 py-2 pb-4"> 
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)}>
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Name</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="E-Commerce" {...field}/>
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                                <Button 
+                                    variant="outline" 
+                                    onClick={storeModal.onClose}>Cancel
+                                </Button>
+                                <Button type="submit">Continue</Button>
+                            </div>
+                        </form>
+                    </Form>
+                </div>
+            </div>
         </Modal>
     )
 }
