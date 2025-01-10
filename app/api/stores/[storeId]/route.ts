f23b6b38d45ff3b32eb9b8c1460bafe05f8fefde
Decaprio22312081 <decodecaprio@gmail.com>
2025-01-11 03:00:28 +0700

    Rico-berfungsi untuk menghapus satu atau lebih entri toko dari database

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -1,26 +1,33 @@
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export async  function PATCH (
+import db from "@/lib/db";
+
+export async  function DELETE (
     req:Request,
     { params }: {params: {storeId: string }}
 ){ 
     try {
         const {userId } = await auth();
-        const body = await req.json();
-
-        const { name } = body;
 
         if (!userId) {
             return new NextResponse("unauthenticated", { status: 401 });
         }
 
-        if (!name){
-            return new NextResponse("Name is required", {status:400})
-        }
+        if (!params.storeId){
+            return new NextResponse("Store id is required", { status:400});
+            }
+
+        const store = await db.store.deleteMany({
+            where: {
+                id: params.storeId,
+                userId
+            }
+        });
         
+        return NextResponse.json(store);
     } catch (error) {
-        console.log('[STORE_PATCH]', error);
+        console.log('[STORE_DELETE]', error);
         return new NextResponse("Internal error", {status: 500})
     }
-}
+};
