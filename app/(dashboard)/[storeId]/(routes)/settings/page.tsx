5524814bf0dceecf2f878c82ee96113ed0ba162d
Decaprio22312081 <decodecaprio@gmail.com>
2025-01-10 18:06:10 +0700

    Rico-membuat sign –in

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,9 +1,40 @@
-const SettingsPage = () =>{
-    return(
-        <div>
-            Hello Settings
-        </div>
-    )
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation";
+
+import db from "@/lib/db";
+
+interface SettingsPageProps {
+    params: {
+        storeId: string;
+    }
 };
 
-export default SettingsPage
+const SettingPage: React.FC<SettingsPageProps> = async ({
+    params
+}) =>{
+    const { userId} = await auth();
+
+    if (!userId) {
+        redirect ("/sign-in");
+    }
+
+    const store = await db.store.findFirst({
+        where: {
+            id: params.storeId,
+            userId
+        }
+    });
+
+    if (!store){
+        redirect("/");
+    }
+    return (
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+            Hello Settings
+        </div>
+        </div>
+    );
+}
+
+export default SettingPage;
