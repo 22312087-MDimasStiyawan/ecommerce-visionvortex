55dabb5fcd23ba1667112f32c30f6b459eb901b1
Decaprio22312081 <decodecaprio@gmail.com>
2025-01-11 01:08:20 +0700

    Rico-Dengan Zod, Anda dapat membuat skema (schemas)

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,32 +1,39 @@
 "use client";
 
+import * as z from "zod";
 import { Store } from "@prisma/client";
 import { Trash } from "lucide-react";
 
 import { Heading } from "@/components/ui/heading";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 
 
 interface SettingsFormProps {
     initialData: Store;
 }
 
+const formSchema = z. object ({
+    name: z.string().min(1)
+});
+
 export const SettingsForm: React.FC<SettingsFormProps> = ({
     initialData
 }) => {
     return (
-       <div className="flex items-center justify-between">
-        <Heading
-        title="Settings"
-        description="Manage store preferences"
-        />
-        <Button
-            variant="destructive"
-            size="icon"
-            onClick={() => {}}
-         >   
-         <Trash className="h-4 w-4" />
-        </Button>
+       <><div className="flex items-center justify-between">
+            <Heading
+                title="Settings"
+                description="Manage store preferences" />
+            <Button
+                variant="destructive"
+                size="icon"
+                onClick={() => { } }
+            >
+                <Trash className="h-4 w-4" />
+            </Button>
         </div>
+        <Separator />
+        </>
     );
 };
