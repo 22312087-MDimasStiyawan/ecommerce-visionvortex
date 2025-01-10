d09ccaf1d84df4d238b511b27c260936d0264ce7
Rifki9830 <rifki_abdullah@teknokrat.ac.id>
2025-01-10 09:27:09 +0700

    Rifki: Merender navigasi dinamis menggunakan data dari array routes pada main-nav

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { cn } from "@/lib/utils"
+import Link from "next/link";
 import { useParams, usePathname } from "next/navigation"
 
 export function MainNav({
@@ -23,7 +24,18 @@ export function MainNav({
             "flex items-center space-x-4 lg:space-x-6",
             className
         )}>
-            This is Main Nav
+            {routes.map((route) => (
+                <Link
+                key={route.href}
+                href={route.href}
+                className={cn(
+                    "text-sm font-medium transition-colors hover:text-primary",
+                    route.active ? "text-black dark:text-white" : "text-muted-foreground"
+                )}
+                >
+                    {route.label}
+                </Link>
+            ))}
         </nav>
     )
 }
