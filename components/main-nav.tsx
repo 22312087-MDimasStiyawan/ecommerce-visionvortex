181c79f10071c320c1c145eb4e4454b21d627f46
Rifki9830 <rifki_abdullah@teknokrat.ac.id>
2025-01-10 08:16:40 +0700

    Rifki: membuat file baru main-nav.tsx dan membuat definisinya lalu dipanggil pada navbar.tsx

---
+++ components/main-nav.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+export function MainNav({
+    className,
+
+}: React.HTMLAttributes<HTMLElement>) {
+    return (
+        <nav>
+            This is Main Nav
+        </nav>
+    )
+}
