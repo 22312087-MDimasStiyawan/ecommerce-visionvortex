d32edd8fccc762948a9bd12f6d8448dfa9c8679f
Decaprio22312081 <decodecaprio@gmail.com>
2025-01-11 01:21:45 +0700

    Rico- useFrom: Mengelola status zodResolver: Mengintegrasikan Zod

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -3,10 +3,13 @@
 import * as z from "zod";
 import { Store } from "@prisma/client";
 import { Trash } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Heading } from "@/components/ui/heading";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+
 
 
 interface SettingsFormProps {
@@ -16,10 +19,16 @@ interface SettingsFormProps {
 const formSchema = z. object ({
     name: z.string().min(1)
 });
+type SettingsFormValues = z. infer<typeof formSchema>;
 
 export const SettingsForm: React.FC<SettingsFormProps> = ({
     initialData
 }) => {
+
+    const form = useForm<SettingsFormValues>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData
+    })
     return (
        <><div className="flex items-center justify-between">
             <Heading
