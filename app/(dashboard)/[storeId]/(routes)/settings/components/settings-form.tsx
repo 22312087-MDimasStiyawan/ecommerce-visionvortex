5e4025255b39359030d74f126e3437584bd5b056
Decaprio22312081 <decodecaprio@gmail.com>
2025-01-10 19:29:29 +0700

    Rico-potongan JSX (JavaScript XML) yang berfungsi untuk membuat struktur HTML dengan bantuan React atau Next.js.

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -2,16 +2,21 @@
 
 import { Store } from "@prisma/client";
 
-interface SettingsPageProps {
+import { Heading } from "@/components/ui/heading";
+
+interface SettingsFormProps {
     initialData: Store;
 }
 
-export const SettingsFrom: React.FC <SettingsPageProps> = ({
+export const SettingsForm: React.FC<SettingsFormProps> = ({
     initialData
 }) => {
     return (
-        <div>
-        Settings From
+       <div className="flex items-center justify-between">
+        <Heading
+        title="Settings"
+        description="Manage store preferences"
+        />
         </div>
     );
 };
