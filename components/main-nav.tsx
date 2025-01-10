305880e2189116774ca2ca482c04041bdef967a3
Rifki9830 <rifki_abdullah@teknokrat.ac.id>
2025-01-10 08:39:38 +0700

    Rifki: Membuat const routes baru pada mainnav

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,11 +1,28 @@
 'use client'
 
+import { cn } from "@/lib/utils"
+import { useParams, usePathname } from "next/navigation"
+
 export function MainNav({
     className,
 
 }: React.HTMLAttributes<HTMLElement>) {
+    const pathname = usePathname();
+    const params = useParams();
+
+    const routes = [
+        {
+            href: `/${params.storeId}/settings`,
+            label: 'settings',
+            active: pathname === `/${params.storeId}/settings`,
+        }
+    ]
+
     return (
-        <nav>
+        <nav className={cn(
+            "flex items-center space-x-4 lg:space-x-6",
+            className
+        )}>
             This is Main Nav
         </nav>
     )
