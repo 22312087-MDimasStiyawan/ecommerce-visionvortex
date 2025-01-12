8bb4b680034698db537201d4262406798c299b27
Rifki9830 <rifki_abdullah@teknokrat.ac.id>
2025-01-12 20:39:09 +0700

    Rifki: Menambahkan tampilan pada billboards berupa heading dan button tambah

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,11 +1,25 @@
 "use client"
 
+import { Plus } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import { Heading } from "@/components/ui/heading"
+import { Separator } from "@/components/ui/separator"
+
 export const BillboardClient = () => {
     return (
         <>
             <div className="flex items-center justify-between">
-                Billboard Client
+                <Heading 
+                    title="Billboards (0)"
+                    description="Manage Billboards for your store"
+                />
+                <Button>
+                    <Plus className="mr-2 h-4 w-4"/>
+                    Add New
+                </Button>
             </div>
+            <Separator />
         </>
     )
 }
