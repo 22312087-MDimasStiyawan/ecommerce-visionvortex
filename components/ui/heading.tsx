b3632f2da41e3d71a818308827b87fd8f458cc91
Mdims75 <stiyawandims@gmail.com>
2025-01-13 12:48:35 +0700

    Dimas: Memperbaiki Tampilan

--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -8,7 +8,7 @@ export const Heading: React.FC<HeadingProps> = ({
 }) => {
     return(
         <div>
-            <h2>{title}</h2>
+            <h2 className="text-3xl font-bold tracking-tight">{title}</h2>
                 <p>
                     {description}
                 </p>
