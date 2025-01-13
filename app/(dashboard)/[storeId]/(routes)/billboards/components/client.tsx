230f654420785cf56b8b16e3b528011d6af36de9
Mdims75 <stiyawandims@gmail.com>
2025-01-13 16:13:34 +0700

    Dimas : Menambahkan Program untuk Menampilkan Pagination dalam table

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -7,7 +7,8 @@ import { Button } from "@/components/ui/button"
 import { Heading } from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
 
-import { BillboardColumn } from "./columns"
+import { BillboardColumn, columns } from "./columns"
+import { DataTable } from "@/components/ui/data-table"
 
 interface BillboardClientProps{
     data: BillboardColumn[]
@@ -33,6 +34,7 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({
                 </Button>
             </div>
             <Separator />
+            <DataTable columns={columns} data={data}/>
         </>
     )
 }
