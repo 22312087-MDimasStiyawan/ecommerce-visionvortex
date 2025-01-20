7d5e772ccc2de6e23a68104ab9d907f2eeefcd0a
Rifki9830 <rifki_abdullah@teknokrat.ac.id>
2025-01-20 22:18:34 +0700

    Rifki: Melakukan return pada categories di client.tsx

--- app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -26,19 +26,19 @@ export const CategoryClient: React.FC<CategoryClientProps> = ({
         <>
             <div className="flex items-center justify-between">
                 <Heading 
-                    title={`Billboards(${data.length})`}
-                    description="Kelola Billboard untuk toko Anda"
+                    title={`Categories(${data.length})`}
+                    description="Kelola Kategori untuk toko Anda"
                 />
-                <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
+                <Button onClick={() => router.push(`/${params.storeId}/categories/new`)}>
                     <Plus className="mr-2 h-4 w-4"/>
                     Add New
                 </Button>
             </div>
             <Separator />
             <DataTable searchKey="label" columns={columns} data={data}/>
-            <Heading title="API" description="API calls for Billboards"/>
+            <Heading title="API" description="API calls for Categories"/>
             <Separator/>
-            <ApiList entityName="billboards" entityIdName="billboardId"/>
+            <ApiList entityName="categories" entityIdName="categoryId"/>
         </>
     )
 }
