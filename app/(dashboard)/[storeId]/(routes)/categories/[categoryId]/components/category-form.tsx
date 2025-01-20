f20d1d8b9990d62cedbe320683485dc3ba040e35
Rifki9830 <rifki_abdullah@teknokrat.ac.id>
2025-01-21 01:50:56 +0700

    Rifki: menambahkan fungsi/memperbaiki const onSubmit dan onDelete

--- app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/category-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/category-form.tsx
@@ -72,12 +72,12 @@ const action = initialData ? "Save change" : "Create";
         try {
             setLoading(true);
             if (initialData) {
-                await axios.patch(`/api/${params.storeId}/billboards/${params.billboardId}`,data);
+                await axios.patch(`/api/${params.storeId}/categories/${params.categoryId}`,data);
             } else {
-                await axios.post(`/api/${params.storeId}/billboards`,data);
+                await axios.post(`/api/${params.storeId}/categories`,data);
             }
             router.refresh();
-            router.push(`/${params.storeId}/billboards`)
+            router.push(`/${params.storeId}/categories`)
             toast.success(toastMessage);
         } catch (error) {
             toast.error("Something went wrong.");
@@ -89,12 +89,12 @@ const action = initialData ? "Save change" : "Create";
     const onDelete = async () => {
         try {
            setLoading(true) 
-           await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
+           await axios.delete(`/api/${params.storeId}/categories/${params.categoryId}`);
            router.refresh();
-           router.push(`/${params.storeId}/billboards`)
-           toast.success("Billboard deleted.")
+           router.push(`/${params.storeId}/categories`)
+           toast.success("Category deleted.")
         } catch (error) {
-            toast.error("Make sure you removed all categories using this billboard first.");
+            toast.error("Make sure you removed all products using this category first.");
         }finally{
             setLoading(false)
             setOpen(false)
