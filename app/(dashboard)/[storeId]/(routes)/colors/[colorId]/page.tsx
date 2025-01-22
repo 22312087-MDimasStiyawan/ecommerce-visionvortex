819921e48fd3d6a302cc0f90b8101054fbe2fc27
Mdims75 <stiyawandims@gmail.com>
2025-01-22 22:57:13 +0700

    Dimas : Mengubah program page.tsx dalam folder colorId

--- app/(dashboard)/[storeId]/(routes)/colors/[colorId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/[colorId]/page.tsx
@@ -1,23 +1,23 @@
 import db from "@/lib/db";
 import { SizeForm } from "./components/size-form";
 
-const SizePage = async ({ params }: { params: { sizeId: string } }) => {
+const ColorPage = async ({ params }: { params: { colorId: string } }) => {
     // Tunggu `params` sebelum mengakses properti `sizeId`
-    const { sizeId } = await params;
+    const { colorId } = await params;
 
-    const size = await db.size.findUnique({
+    const color = await db.color.findUnique({
         where: {
-            id: sizeId, // Gunakan sizeId setelah params di-await
+            id: colorId, // Gunakan sizeId setelah params di-await
         },
     });
 
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                <SizeForm initialData={size} />
+                <SizeForm initialData={color} />
             </div>
         </div>
     );
 };
 
-export default SizePage;
+export default ColorPage;
