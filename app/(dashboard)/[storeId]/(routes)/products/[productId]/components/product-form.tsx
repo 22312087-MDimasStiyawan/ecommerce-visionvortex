9ae32d8a0f4c6d845291ad1b61e8f3748eea38e0
Rifki9830 <rifki_abdullah@teknokrat.ac.id>
2025-01-23 22:42:58 +0700

    Rifki: Menambahkan FormField berupa select color

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
@@ -264,7 +264,42 @@ const action = initialData ? "Simpan Product" : "Buat Product";
                             </Select>
                             <FormMessage />
                         </FormItem>
-                    )}
+                        )}
+                    />
+                    <FormField
+                    control={form.control}
+                    name="colorId"
+                    render={({field}) => (
+                        <FormItem>
+                            <FormLabel>Color</FormLabel>
+                            <Select 
+                                disabled={loading} 
+                                onValueChange={field.onChange} 
+                                value={field.value} 
+                                defaultValue={field.value}
+                            >
+                                <FormControl>
+                                    <SelectTrigger>
+                                        <SelectValue 
+                                            defaultValue={field.value}
+                                            placeholder="Select a Color"
+                                        />
+                                    </SelectTrigger>
+                                </FormControl>
+                                <SelectContent>
+                                    {colors.map((color) => (
+                                        <SelectItem
+                                            key={color.id}
+                                            value={color.id}
+                                        >
+                                            {color.name}
+                                        </SelectItem>
+                                    ))}
+                                </SelectContent>
+                            </Select>
+                            <FormMessage />
+                        </FormItem>
+                        )}
                     />
                 </div>
                 <Button disabled={loading} className="ml-auto" type="submit">
