551844502d52923b88f8d9a15cdd67c64c9c78ec
Rifki9830 <rifki_abdullah@teknokrat.ac.id>
2025-01-23 19:16:49 +0700

    Rifki: Merubah Page, Client, Column pada product

--- app/(dashboard)/[storeId]/(routes)/products/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -1,36 +1,48 @@
 import { format, formatDate } from "date-fns"
 
 import db from "@/lib/db";
-import { BillboardClient } from "./components/client";
-import { BillboardColumn } from "./components/columns";
+import { formatter } from "@/lib/utils";
+import { ProductClient } from "./components/client";
+import { ProductColumn } from "./components/columns";
 
-const BillboardsPage = async ({
+const ProductsPage = async ({
     params
 }: {
     params: {storeId: string}
 }) => {
-    const billboards= await db.billboard.findMany({
+    const products= await db.product.findMany({
         where: {
             storeId: params.storeId
+        },
+        include: {
+            category: true,
+            size: true,
+            color: true,
         },
         orderBy: {
             createdAt: 'desc'
         }
     });
 
-    const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+    const formattedProducts: ProductColumn[] = products.map((item) => ({
         id: item.id,
-        label: item.label,
+        name: item.name,
+        isFeatured: item.isFeatured,
+        isArchived: item.isArchived,
+        price: formatter.format(item.price.toNumber()),
+        category: item.category.name,
+        size: item.category.name,
+        color: item.color.value,
         createdAt: formatDate(item.createdAt,"MMMM do, yyyy" )
     }));
 
     return (  
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                <BillboardClient data={formattedBillboards}/>
+                <ProductClient data={formattedProducts}/>
             </div>
         </div>
     );
 }
  
-export default BillboardsPage;
+export default ProductsPage;
